c3c9b5c7c11de23d3303dd69d4eb6002866ec771
docia <matiushenko107@gmail.com>
2023-01-28 22:18:25 +0200

    task:  access a property which is stored as the value of a variable

--- freeCodeCamp/freeCodeCamp.js
+++ freeCodeCamp/freeCodeCamp.js
@@ -1,21 +1,21 @@
-// Setup
+
 let lastNameLength = 0;
 const lastName = "Lovelace";
 
-//  Only change code below this line
+
 lastNameLength = lastName.length;
 
-// Setup
+
 const lastName1 = "Lovelace";
 
-// Only change code below this line
+
 const thirdLetterOfLastName = lastName1[2];
 
-// Setup
+
 const lastName2 = "Lovelace";
 
-// Only change code below this line
-const lastLetterOfLastName = lastName2[lastName2.length - 1]; // Change this line
+
+const lastLetterOfLastName = lastName2[lastName2.length - 1]; 
 
 const myArray = [
 	[1, 2, 3],
@@ -26,30 +26,30 @@ const myArray = [
 
 const myData = myArray[2][1];
 
-// Setup
+
 const myArray1 = [
 	["John", 23],
 	["cat", 2],
 ];
 
-// Only change code below this line
+
 myArray1.push(["dog", 3]);
 
-// Setup
+
 const myArray2 = [
 	["John", 23],
 	["cat", 2],
 ];
 const removedFromMyArray2 = myArray2.pop();
 
-// Setup
+
 const myArray3 = [
 	["John", 23],
 	["dog", 3],
 ];
 const removedFromMyArray = myArray3.shift();
 
-// Setup
+
 const myArray4 = [
 	["John", 23],
 	["dog", 3],
@@ -58,7 +58,7 @@ myArray4.shift();
 myArray4.unshift(["Paul", 35]);
 
 function myLocalScope() {
-	// Only change code below this line
+	
 	let myVar;
 
 	console.log("inside myLocalScope", myVar);
@@ -69,43 +69,43 @@ myLocalScope();
 // myVar is not defined outside of myLocalScope
 // console.log('outside myLocalScope is undefined ', myVar === undefined);
 
-// Setup
+
 const outerWear = "T-Shirt";
 
 function myOutfit() {
-	// Only change code below this line
+	
 	const outerWear = "sweater";
-	// Only change code above this line
+	
 	return outerWear;
 }
 
 myOutfit();
 
-// Setup
+
 let sum = 0;
 
 function addThree() {
 	sum = sum + 3;
 }
 
-// Only change code below this line
+
 function addFive() {
 	sum += 5;
 }
 
-// Only change code above this line
+
 
 addThree();
 addFive();
 
-// Setup
+
 let processed = 0;
 
 function processArg(num) {
 	return (num + 3) / 5;
 }
 
-// Only change code below this line
+
 processArg(7);
 processed = processArg(7);
 
@@ -116,35 +116,35 @@ processed = processArg(7);
 // The nextInLine function should then return the element that was removed.
 
 function nextInLine(arr, item) {
-	// Only change code below this line
+	
 	arr.push(item);
 	item = arr.shift();
 	return item;
-	// Only change code above this line
+	
 }
 
-// Setup
+
 let testArr = [1, 2, 3, 4, 5];
 
-// Display code
+
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
 
 function trueOrFalse(wasThatTrue) {
-	// Only change code below this line
+	
 	if (wasThatTrue) {
 		return "Yes, that was true";
 	}
 	return "No, that was false";
 
-	// Only change code above this line
+	
 }
 
-// Setup
+
 function testEqual(val) {
 	if (val == 12) {
-		// Change this line
+		
 		return "Equal";
 	}
 	return "Not Equal";
@@ -153,26 +153,26 @@ function testEqual(val) {
 testEqual(10);
 
 function testLogicalAnd(val) {
-	// Only change code below this line
+	
 
 	if (val >= 25 && val <= 50) {
 		return "Yes";
 	}
 
-	// Only change code above this line
+	
 	return "No";
 }
 
 testLogicalAnd(10);
 
 function testLogicalOr(val) {
-	// Only change code below this line
+	
 
 	if (val < 10 || val > 20) {
 		return "Outside";
 	}
 
-	// Only change code above this line
+	
 	return "Inside";
 }
 
@@ -180,7 +180,7 @@ testLogicalOr(15);
 
 function caseInSwitch(val) {
 	let answer = "";
-	// Only change code below this line
+	
 	switch (val) {
 		case 1:
 			answer = "alpha";
@@ -196,7 +196,7 @@ function caseInSwitch(val) {
 			break;
 	}
 
-	// Only change code above this line
+	
 	return answer;
 }
 
@@ -204,7 +204,7 @@ caseInSwitch(1);
 
 function switchOfStuff(val) {
 	let answer = "";
-	// Only change code below this line
+	
 	switch (val) {
 		case "a":
 			answer = "apple";
@@ -218,7 +218,7 @@ function switchOfStuff(val) {
 		default:
 			answer = "stuff";
 	}
-	// Only change code above this line
+	
 	return answer;
 }
 
@@ -226,7 +226,7 @@ switchOfStuff(1);
 
 function chainToSwitch(val) {
 	let answer = "";
-	// Only change code below this line
+	
 
 	switch (val) {
 		case "bob":
@@ -246,7 +246,7 @@ function chainToSwitch(val) {
 			break;
 	}
 
-	// Only change code above this line
+	
 	return answer;
 }
 
@@ -267,7 +267,7 @@ console.log(abTest(-2, 2));
 let count = 0;
 
 function cc(card) {
-  // Only change code below this line
+  
 switch(card) {
   case 2:
   case 3:
@@ -290,8 +290,19 @@ switch(card) {
     return count + " Hold";
   }
  
-  // Only change code above this line
 }
 
 cc(2); cc(3); cc(7); cc('K'); cc('A');
 console.log(cc(10, 'J', 'Q', 'K', 'A'));
+
+
+
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+
+const playerNumber = 16;  
+const player = testObj[playerNumber];   
