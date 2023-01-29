d9afa061e3c04bd1695c189e99b248ea627ccab0
docia <matiushenko107@gmail.com>
2023-01-29 10:53:14 +0200

    task: Using Objects for Lookups

--- freeCodeCamp/freeCodeCamp.js
+++ freeCodeCamp/freeCodeCamp.js
@@ -306,3 +306,22 @@ const testObj = {
 
 const playerNumber = 16;  
 const player = testObj[playerNumber];   
+
+
+function phoneticLookup(val) {
+  let result = "";
+
+  let lookup = {
+   "alpha":"Adams",
+   "bravo":"Boston",
+   "charlie":"Chicago",
+   "delta":"Denver",
+   "echo":"Easy",
+   "foxtrot":"Frank"
+  }
+  
+  result = lookup[val];
+  return result;
+}
+
+console.log(phoneticLookup("bravo"));
