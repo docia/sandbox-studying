849ec3b97e2ba56a9f6e182524cd24027e864575
docia <matiushenko107@gmail.com>
2023-01-30 11:36:05 +0200

    task: Accessing Nested Arrays
    find a second item in the trees list from the myPlants object.

--- freeCodeCamp/freeCodeCamp.js
+++ freeCodeCamp/freeCodeCamp.js
@@ -375,3 +375,24 @@ const myStorage = {
 };
 
 const gloveBoxContents = myStorage.car.inside['glove box'];
+
+const myPlants = [
+  {
+    type: "flowers",
+    list: [
+      "rose",
+      "tulip",
+      "dandelion"
+    ]
+  },
+  {
+    type: "trees",
+    list: [
+      "fir",
+      "pine",
+      "birch"
+    ]
+  }
+];
+
+const secondTree = myPlants[1].list[1];
