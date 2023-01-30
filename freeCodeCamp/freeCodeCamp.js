af14463f477cdf972a9169c58673cc5818359d95
docia <matiushenko107@gmail.com>
2023-01-30 11:20:18 +0200

    task: Accessing Nested Objects

--- freeCodeCamp/freeCodeCamp.js
+++ freeCodeCamp/freeCodeCamp.js
@@ -361,3 +361,17 @@ const myMusic = [
     "gold": true
   }
 ];
+
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside['glove box'];
