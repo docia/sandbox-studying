5511b706430017d15d3433d03f575b2985ea26bd
docia <matiushenko107@gmail.com>
2023-02-01 18:07:48 +0200

    task: loop through the array and sum all values

--- freeCodeCamp/freeCodeCamp.js
+++ freeCodeCamp/freeCodeCamp.js
@@ -1,21 +1,15 @@
-
 let lastNameLength = 0;
 const lastName = "Lovelace";
 
-
 lastNameLength = lastName.length;
-
 
 const lastName1 = "Lovelace";
 
-
 const thirdLetterOfLastName = lastName1[2];
-
 
 const lastName2 = "Lovelace";
 
-
-const lastLetterOfLastName = lastName2[lastName2.length - 1]; 
+const lastLetterOfLastName = lastName2[lastName2.length - 1];
 
 const myArray = [
 	[1, 2, 3],
@@ -26,15 +20,12 @@ const myArray = [
 
 const myData = myArray[2][1];
 
-
 const myArray1 = [
 	["John", 23],
 	["cat", 2],
 ];
 
-
 myArray1.push(["dog", 3]);
-
 
 const myArray2 = [
 	["John", 23],
@@ -42,13 +33,11 @@ const myArray2 = [
 ];
 const removedFromMyArray2 = myArray2.pop();
 
-
 const myArray3 = [
 	["John", 23],
 	["dog", 3],
 ];
 const removedFromMyArray = myArray3.shift();
-
 
 const myArray4 = [
 	["John", 23],
@@ -58,7 +47,6 @@ myArray4.shift();
 myArray4.unshift(["Paul", 35]);
 
 function myLocalScope() {
-	
 	let myVar;
 
 	console.log("inside myLocalScope", myVar);
@@ -69,18 +57,15 @@ myLocalScope();
 // myVar is not defined outside of myLocalScope
 // console.log('outside myLocalScope is undefined ', myVar === undefined);
 
-
 const outerWear = "T-Shirt";
 
 function myOutfit() {
-	
 	const outerWear = "sweater";
-	
+
 	return outerWear;
 }
 
 myOutfit();
-
 
 let sum = 0;
 
@@ -88,23 +73,18 @@ function addThree() {
 	sum = sum + 3;
 }
 
-
 function addFive() {
 	sum += 5;
 }
 
-
-
 addThree();
 addFive();
-
 
 let processed = 0;
 
 function processArg(num) {
 	return (num + 3) / 5;
 }
-
 
 processArg(7);
 processed = processArg(7);
@@ -116,35 +96,26 @@ processed = processArg(7);
 // The nextInLine function should then return the element that was removed.
 
 function nextInLine(arr, item) {
-	
 	arr.push(item);
 	item = arr.shift();
 	return item;
-	
 }
 
-
 let testArr = [1, 2, 3, 4, 5];
-
 
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
 
 function trueOrFalse(wasThatTrue) {
-	
 	if (wasThatTrue) {
 		return "Yes, that was true";
 	}
 	return "No, that was false";
-
-	
 }
-
 
 function testEqual(val) {
 	if (val == 12) {
-		
 		return "Equal";
 	}
 	return "Not Equal";
@@ -153,26 +124,20 @@ function testEqual(val) {
 testEqual(10);
 
 function testLogicalAnd(val) {
-	
-
 	if (val >= 25 && val <= 50) {
 		return "Yes";
 	}
 
-	
 	return "No";
 }
 
 testLogicalAnd(10);
 
 function testLogicalOr(val) {
-	
-
 	if (val < 10 || val > 20) {
 		return "Outside";
 	}
 
-	
 	return "Inside";
 }
 
@@ -180,7 +145,7 @@ testLogicalOr(15);
 
 function caseInSwitch(val) {
 	let answer = "";
-	
+
 	switch (val) {
 		case 1:
 			answer = "alpha";
@@ -196,7 +161,6 @@ function caseInSwitch(val) {
 			break;
 	}
 
-	
 	return answer;
 }
 
@@ -204,7 +168,7 @@ caseInSwitch(1);
 
 function switchOfStuff(val) {
 	let answer = "";
-	
+
 	switch (val) {
 		case "a":
 			answer = "apple";
@@ -218,7 +182,7 @@ function switchOfStuff(val) {
 		default:
 			answer = "stuff";
 	}
-	
+
 	return answer;
 }
 
@@ -226,7 +190,6 @@ switchOfStuff(1);
 
 function chainToSwitch(val) {
 	let answer = "";
-	
 
 	switch (val) {
 		case "bob":
@@ -246,7 +209,6 @@ function chainToSwitch(val) {
 			break;
 	}
 
-	
 	return answer;
 }
 
@@ -263,166 +225,145 @@ function abTest(a, b) {
 abTest(2, 2);
 console.log(abTest(-2, 2));
 
-
 let count = 0;
 
 function cc(card) {
-  
-switch(card) {
-  case 2:
-  case 3:
-  case 4:
-  case 5:
-  case 6:
-    count ++;
-    break;
-  case 10:
-  case 'J':
-  case 'Q':
-  case 'K':
-  case 'A':
-    count --; 
-    break;
-  }
-  if ( count > 0){
-    return count + " Bet";
-  } else {
-    return count + " Hold";
-  }
- 
+	switch (card) {
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+			count++;
+			break;
+		case 10:
+		case "J":
+		case "Q":
+		case "K":
+		case "A":
+			count--;
+			break;
+	}
+	if (count > 0) {
+		return count + " Bet";
+	} else {
+		return count + " Hold";
+	}
 }
 
-cc(2); cc(3); cc(7); cc('K'); cc('A');
-console.log(cc(10, 'J', 'Q', 'K', 'A'));
-
-
+cc(2);
+cc(3);
+cc(7);
+cc("K");
+cc("A");
+console.log(cc(10, "J", "Q", "K", "A"));
 
 const testObj = {
-  12: "Namath",
-  16: "Montana",
-  19: "Unitas"
+	12: "Namath",
+	16: "Montana",
+	19: "Unitas",
 };
 
-
-const playerNumber = 16;  
-const player = testObj[playerNumber];   
-
+const playerNumber = 16;
+const player = testObj[playerNumber];
 
 function phoneticLookup(val) {
-  let result = "";
+	let result = "";
 
-  let lookup = {
-   "alpha":"Adams",
-   "bravo":"Boston",
-   "charlie":"Chicago",
-   "delta":"Denver",
-   "echo":"Easy",
-   "foxtrot":"Frank"
-  }
-  
-  result = lookup[val];
-  return result;
+	let lookup = {
+		alpha: "Adams",
+		bravo: "Boston",
+		charlie: "Chicago",
+		delta: "Denver",
+		echo: "Easy",
+		foxtrot: "Frank",
+	};
+
+	result = lookup[val];
+	return result;
 }
 
-console.log(phoneticLookup("bravo")); 
-
+console.log(phoneticLookup("bravo"));
 
 function checkObj(obj, checkProp) {
-
-  if (obj.hasOwnProperty(checkProp)) {
-    return obj[checkProp];
-  } return "Not Found";
+	if (obj.hasOwnProperty(checkProp)) {
+		return obj[checkProp];
+	}
+	return "Not Found";
 }
 
 checkObj({ gift: "pony", pet: "kitten", bed: "sleigh" });
 console.log(checkObj("gift", "pony"));
 
 const myMusic = [
-   {
-     artist:"Bob Marley",
-     title: "Song",
-     release_year: 1290,
-     formats: [
-       "CD",
-       "8T",
-       "LP"
-     ]
-
-   },
-  {
-    "artist": "Billy Joel",
-    "title": "Piano Man",
-    "release_year": 1973,
-    "formats": [
-      "CD",
-      "8T",
-      "LP"
-    ],
-    "gold": true
-  }
+	{
+		artist: "Bob Marley",
+		title: "Song",
+		release_year: 1290,
+		formats: ["CD", "8T", "LP"],
+	},
+	{
+		artist: "Billy Joel",
+		title: "Piano Man",
+		release_year: 1973,
+		formats: ["CD", "8T", "LP"],
+		gold: true,
+	},
 ];
 
 const myStorage = {
-  "car": {
-    "inside": {
-      "glove box": "maps",
-      "passenger seat": "crumbs"
-     },
-    "outside": {
-      "trunk": "jack"
-    }
-  }
+	car: {
+		inside: {
+			"glove box": "maps",
+			"passenger seat": "crumbs",
+		},
+		outside: {
+			trunk: "jack",
+		},
+	},
 };
 
-const gloveBoxContents = myStorage.car.inside['glove box'];
+const gloveBoxContents = myStorage.car.inside["glove box"];
 
 const myPlants = [
-  {
-    type: "flowers",
-    list: [
-      "rose",
-      "tulip",
-      "dandelion"
-    ]
-  },
-  {
-    type: "trees",
-    list: [
-      "fir",
-      "pine",
-      "birch"
-    ]
-  }
+	{
+		type: "flowers",
+		list: ["rose", "tulip", "dandelion"],
+	},
+	{
+		type: "trees",
+		list: ["fir", "pine", "birch"],
+	},
 ];
 
 const secondTree = myPlants[1].list[1];
 
 // Setup
 const recordCollection = {
-  2548: {
-    albumTitle: 'Slippery When Wet',
-    artist: 'Bon Jovi',
-    tracks: ['Let It Rock', 'You Give Love a Bad Name']
-  },
-  2468: {
-    albumTitle: '1999',
-    artist: 'Prince',
-    tracks: ['1999', 'Little Red Corvette']
-  },
-  1245: {
-    artist: 'Robert Palmer',
-    tracks: []
-  },
-  5439: {
-    albumTitle: 'ABBA Gold'
-  }
+	2548: {
+		albumTitle: "Slippery When Wet",
+		artist: "Bon Jovi",
+		tracks: ["Let It Rock", "You Give Love a Bad Name"],
+	},
+	2468: {
+		albumTitle: "1999",
+		artist: "Prince",
+		tracks: ["1999", "Little Red Corvette"],
+	},
+	1245: {
+		artist: "Robert Palmer",
+		tracks: [],
+	},
+	5439: {
+		albumTitle: "ABBA Gold",
+	},
 };
 
 // // Only change code below this line
 // function updateRecords(records, id, prop, value) {
 //   if (prop != tracks || value != ''){
 //     prop = value;
-//   } 
+//   }
 //   if (prop == tracks && id.hasOwnProperty(tracks) == false) {
 //     let arr = new Array();
 //     arr.push(value);
@@ -441,25 +382,34 @@ const descendingArray = [];
 let i = 5;
 
 while (i >= 0) {
-  descendingArray.push(i);
-  i--;
+	descendingArray.push(i);
+	i--;
 }
 console.log(descendingArray);
 
 const increasingArray = [];
-for (let i = 0; i < 6; i++){
-  increasingArray.push(i);
+for (let i = 0; i < 6; i++) {
+	increasingArray.push(i);
 }
 console.log(increasingArray);
 
 const oddIncreasingArray = [];
-for ( let i = 1; i < 10; i +=2){
-    oddIncreasingArray.push(i);
+for (let i = 1; i < 10; i += 2) {
+	oddIncreasingArray.push(i);
 }
 console.log(herArray);
 
 const oddDescendingArray = [];
-for (let i = 9; i > 0; i -= 2){
-  oddDescendingArray.push(i);
+for (let i = 9; i > 0; i -= 2) {
+	oddDescendingArray.push(i);
 }
 console.log(oddDescendingArray);
+
+
+const myArr = [2, 3, 4, 5, 6];
+let total = 0;
+
+for (let i = 0; i < myArr.length; i++) {
+	total += myArr[i];
+	console.log(total);
+}
