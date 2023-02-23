10ab8f2d0e0e3c55b1327c176b2aecbe72bcf0d2
docia <matiushenko107@gmail.com>
2023-02-23 21:42:01 +0200

    task: function calculator
    create a function-calculator which creates an object with three methods.

--- freeCodeCamp/freeCodeCamp.js
+++ freeCodeCamp/freeCodeCamp.js
@@ -1100,6 +1100,24 @@ let calculator = {
 // alert(calculator.mul());
 
 //----------------------
+function Calculator() {
+	this.read = function () {
+		this.val1 = +prompt("val1?", "");
+		this.val2 = +prompt("val2?", "");
+	};
+	this.sum = function () {
+		return this.val1 + this.val2;
+	};
+	this.mul = function () {
+		return this.val1 * this.val2;
+	}
+}
+let calculator = new Calculator();
+calculator.read();
+alert("Sum=" + calculator.sum());
+alert( "Mul=" + calculator.mul() );
+
+//----------------------
 // наслідування/прототип
 //----------------------
 let baby = { scream: true };
